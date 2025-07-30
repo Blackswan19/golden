e1fcc32f500fb3002ab03b2d64f0897fd4d5243d
Bs label <163457658+Blackswan19@users.noreply.github.com>
2025-07-31 00:32:59 +0530

    Update accverify.js

--- FM/accverify.js
+++ FM/accverify.js
@@ -28,7 +28,7 @@ const passwords = {
                 membershipType: "Premium - Mini",
                 membershipIcon: "https://cdn-icons-png.flaticon.com/512/7641/7641727.png",
                 fineRate: 5,
-                profileBackground: "#ffd900",
+                profileBackground: "#ff8300",
                 stars: 100,
                 loans: [
                     {
