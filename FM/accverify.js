21180c33aaad3997fa3cc10317089914144564c6
Bs label <163457658+Blackswan19@users.noreply.github.com>
2025-08-17 22:04:37 +0530

    Update accverify.js

--- FM/accverify.js
+++ FM/accverify.js
@@ -261,7 +261,7 @@ const passwords = {
                     const endDateStr = `${endMatch[1]}-${endMatch[2]}-${endMatch[3]}`;
                     if (endDateStr === todayStr && !loan.endDate.includes('Overdue')) {
                         document.getElementById("reminderMessage").textContent = 
-                            `Dear ${user.name}, Today(${cleanEndDate}) you have a amount to return which you have taken from the servive ${loan.takenFrom}.Try to clear on time if not overdue fine amount will be added. Thank you!`;
+                            `Dear ${user.name}, Today(${cleanEndDate}) you have a amount to return which you have taken from the servive (${loan.takenFrom}).Try to clear on time if not overdue fine amount will be added. Thank you!`;
                         document.getElementById("reminderModal").style.display = "flex";
                     }
                 }
