d71c0014c213d84833f743a0eff0b3fe72c1b575
Bs label <163457658+Blackswan19@users.noreply.github.com>
2025-09-01 00:26:46 +0530

    Update accverify.js

--- FM/accverify.js
+++ FM/accverify.js
@@ -29,7 +29,7 @@ const passwords = {
                 membershipIcon: "https://cdn-icons-png.flaticon.com/512/7641/7641727.png",
                 fineRate: 20,
                 profileBackground: "red",
-                stars: 100,
+                stars: 0,
                 loans: [
                     {
                         planDate: "10-08-2025",
@@ -463,3 +463,4 @@ const passwords = {
             });
             generateImage(text.trim(), 'all_total_amounts.png');
         }
+
