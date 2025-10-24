0540fd7e8d55dcf325a92d8425a07ad4c570fa07
Bs label <163457658+Blackswan19@users.noreply.github.com>
2025-10-24 22:13:22 +0530

    Update accverify.js

--- FM/accverify.js
+++ FM/accverify.js
@@ -501,7 +501,7 @@ function displayLoanDetails(loan, index) {
             <h3>Total to Return</h3>
             <p><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f"><path d="M480-400 40-640l440-240 440 240-440 240Zm0 160L63-467l84-46 333 182 333-182 84 46-417 227Zm0 160L63-307l84-46 333 182 333-182 84 46L480-80Zm0-411 273-149-273-149-273 149 273 149Zm0-149Z"/></svg>Full Amount : ${totalReturnAmount} Rupees</p>
             <hr>
-            <div class="issuebtn" style="padding-top: 20px;">  
+            <div class="issuebtn" style="padding-top: 20px;width: 102.5%;">  
                 <a target="_blank" href="https://forms.gle/RzTJ8W9bwmm8DVj2A"><button>I have an issue here.!</button></a>
             </div>
         </div>
