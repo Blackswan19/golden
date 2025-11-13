f7812195958ab828410c458a5593001ca836a34b
Bs label <163457658+Blackswan19@users.noreply.github.com>
2025-11-13 17:30:55 +0530

    Update accverify.js

--- FM/accverify.js
+++ FM/accverify.js
@@ -59,9 +59,9 @@ const passwords = {
             url: "https://mfi0212.github.io/swan/offer/programx"
         },
         loans: [
-            { planDate: "29-09-2025", endDate: "13-11-2025<br> <p style='color: orangered;'>(Extra interest added)</p>", interest: 620, takenAmount: 4200, takenFrom: "MLLD", fineRate: 50 },
             { planDate: "01-06-2025", endDate: "16-11-2025(Extended to 39 days)", interest: 2000, takenAmount: 11520, takenFrom: "ProgramsX & Split Pay offer", fineRate: 50 },
-            { planDate: "25-05-2025", endDate: "21-11-2025(Extended to 15 days)", interest: 660, takenAmount: 5100, takenFrom: "Delayit offer", fineRate: 50 }
+            { planDate: "25-05-2025", endDate: "21-11-2025(Extended to 15 days)", interest: 680, takenAmount: 5100, takenFrom: "Delayit offer", fineRate: 50 },
+            { planDate: "29-09-2025", endDate: "28-11-2025(Extended to 15 days)", interest: 680, takenAmount: 4200, takenFrom: "MLLD", fineRate: 40 },
         ]
     },
     "Cherish@123": {
@@ -734,4 +734,3 @@ function showCoinsCount() {
     const coins = document.getElementById('coinsCount').textContent.trim();
     alert(`BsRora(Bot) : \n\nYou have ${coins} rate exchange coins on your account.`);
 }
-
