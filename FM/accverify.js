53c406f0056f63208abba23b35e83270b3588334
Bs label <163457658+Blackswan19@users.noreply.github.com>
2025-11-13 17:45:34 +0530

    Update accverify.js

--- FM/accverify.js
+++ FM/accverify.js
@@ -61,7 +61,7 @@ const passwords = {
         loans: [
             { planDate: "01-06-2025", endDate: "16-11-2025(Extended to 39 days)", interest: 2000, takenAmount: 11520, takenFrom: "ProgramsX & Split Pay offer", fineRate: 50 },
             { planDate: "25-05-2025", endDate: "21-11-2025(Extended to 15 days)", interest: 680, takenAmount: 5100, takenFrom: "Delayit offer", fineRate: 50 },
-            { planDate: "29-09-2025", endDate: "28-11-2025(Extended to 15 days)", interest: 640, takenAmount: 4200, takenFrom: "MLLD", fineRate: 40 },
+            { planDate: "29-09-2025", endDate: "28-11-2025(Extended to 15 days)", interest: 1200, takenAmount: 4880, takenFrom: "MLLD", fineRate: 40 },
         ]
     },
     "Cherish@123": {
