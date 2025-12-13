839fb96c9c7dfcfa85a44d18386daca333603d20
Bs label <163457658+Blackswan19@users.noreply.github.com>
2025-12-13 10:51:06 +0530

    Update changable.js

--- FM/changable.js
+++ FM/changable.js
@@ -3,4 +3,15 @@
  const specialRates = {
             'Thu Dec 04 2025': 34
         };
+function getDailyInterestRate() {
+    const today = new Date().toISOString().slice(0, 10,10); 
+
+    let hash = 5381;
+    for (let i = 0; i < today.length; i++) {
+        hash = ((hash << 5) + hash) + today.charCodeAt(i); 
+    }
+
+    const rate = 40 + (Math.abs(hash) % 15);
+    return rate;
+}
 
