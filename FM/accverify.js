d59e9e1362be5cccbd018b5be894698b9bf7462b
Bs label <163457658+Blackswan19@users.noreply.github.com>
2026-07-23 19:09:18 +0530

    Update accverify.js

--- FM/accverify.js
+++ FM/accverify.js
@@ -42,7 +42,7 @@ const passwords = {
         },
         loans: [
             { planDate: "11-05-2026", endDate: "10-09-2026", interest: 7355, takenAmount: 29418, takenFrom: "Tomar Juntos", fineRate: 130 },
-            { planDate: "21-07-2026", endDate: "21-08-2026", interest: 607, takenAmount: 2425, takenFrom: "Tomar Juntos", fineRate: 130 },
+            { planDate: "21-07-2026", endDate: "21-08-2026", interest: 727, takenAmount: 2425, takenFrom: "Tomar Juntos", fineRate: 130 },
         ]
     },
     "0212": {
