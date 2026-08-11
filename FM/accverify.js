e74594acee592eebbede2bd181fe1e433fb5cd93
Bs label <163457658+Blackswan19@users.noreply.github.com>
2026-08-11 12:56:32 +0530

    Update accverify.js

--- FM/accverify.js
+++ FM/accverify.js
@@ -43,7 +43,7 @@ const passwords = {
             value: "programXoffer.png",
             url: ""
         },
-        showSpecialNotice: "yes",
+        showSpecialNotice: "no",
         specialNoticeText: "Mr. Mahesh Muthinti, Your account points are very low. Use your points earner  or the mining bot, to earn points",
         loans: [
             { planDate: "11-05-2026", endDate: "10-09-2026", interest: 7355, takenAmount: 29418, takenFrom: "Tomar Juntos", fineRate: 130 },
